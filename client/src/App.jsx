23c07b405889548be28edd6c90288e19f68961b0
tribikram <sentribikram@gmail.com>
2024-08-30 22:50:53 +0545

    add react-router-dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { SharedLayout } from "./pages";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <SharedLayout />,
+    children: [
+      // Routes 
+    ]
+  }
+])
 
 function App() {
 
   return (
-    <>
-      Hello Artist
-    </>
+    <RouterProvider router={router} />
   );
 }
 
