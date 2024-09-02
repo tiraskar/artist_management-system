dec648ca716a21035b055bbeddc3cf82e54b4712
tribikram <sentribikram@gmail.com>
2024-09-02 22:07:23 +0545

    custom field for input

--- client/src/components/CustomFormField.jsx
+++ client/src/components/CustomFormField.jsx
@@ -1,41 +1,74 @@
 import PropTypes from "prop-types";
 
+export const inputField = {
+  TEXT: 'TEXT',
+  RADIO: 'RADIO',
+}
+
 const CustomFormField = ({
   props,
+  name,
   label,
   className,
   type = "text",
   handleChange,
   required = true,
-  labelColor = 'black'
+  labelColor = 'black',
+  placeholder,
+  inputType,
+  value
 }) => {
+
+  const renderInput = () => {
+    switch (inputType) {
+      case "RADIO":
+        return (
+          <input type="radio"
+            {...props}
+            name={name}
+            value={value}
+            className='h-4 w-4'
+          />
+        );
+
+      default:
+        return (
+          <input
+            name={name}
+            {...props}
+            type={type}
+            onChange={handleChange}
+            className={`default-input ${className}`}
+            required={required}
+            placeholder={placeholder}
+            value={value}
+          />
+        );
+    }
+  };
 
   return (
     <div className="flex flex-col gap-y-2">
       <label className={`flex text-lg text-${labelColor}`}>
         {label}
       </label>
-      <input
-        name={name}
-        {...props}
-        type={type}
-        onChange={handleChange}
-        className={`default-input ${className}`}
-        required={required}
-      />
+      {renderInput()}
     </div>
   );
 };
 
 CustomFormField.propTypes = {
-  props: PropTypes.object.isRequired,
+  props: PropTypes.object,
+  name: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
   type: PropTypes.string,
-  control: PropTypes.object,
   handleChange: PropTypes.func,
   required: PropTypes.bool,
-  labelColor: PropTypes.string
+  labelColor: PropTypes.string,
+  placeholder: PropTypes.string,
+  inputType: PropTypes.string,
+  value: PropTypes.any,
 };
 
 export default CustomFormField;
