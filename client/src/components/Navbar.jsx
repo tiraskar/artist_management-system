a53f676c30786a09c4ccfbd2cc20ce68bbf4d38d
tribikram <sentribikram@gmail.com>
2024-09-06 23:23:37 +0545

    add: dropdown for login user

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,34 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { UserCircle } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
-    <div className="fixed top-0 left-0 w-full h-12  bg-white border-b-[1px] border-black border-opacity-10 z-40">
-      {/* Navbar content */}
+    <div className="fixed top-0 left-0 w-full h-12 flex justify-between items-center bg-white border-b-[1px] border-black border-opacity-10 z-40 px-10">
+
+      <div className="text-xl font-bold">Dashboard</div>
+      <p className="text-lg tracking-tight font-semibold">Artist Management System</p>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="flex items-center space-x-2 border-[1px] rounded-md px-4 py-1" >
+          <span>Tribikram Sen</span>
+          <UserCircle size={16} />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Change password</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => navigate('/login')} className="cursor-pointer">Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 };
