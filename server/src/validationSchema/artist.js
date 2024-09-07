37c8244fa6bbb43031a7aaa055d376a600e954a1
tribikram <sentribikram@gmail.com>
2024-09-08 00:41:26 +0545

    validation schema to update artist

--- server/src/validationSchema/artist.js
+++ server/src/validationSchema/artist.js
@@ -8,3 +8,13 @@ export const createArtistSchema = Joi.object({
   first_release_year: Joi.number().optional(),
   no_of_albums_released: Joi.number().optional(),
 });
+
+export const updateArtistSchema = Joi.object({
+  artist_id: Joi.number().required(),
+  name: Joi.string().required(),
+  dob: Joi.date().required(),
+  gender: Joi.string().valid("m", "f", "o").required(),
+  address: Joi.string().required(),
+  first_release_year: Joi.number().optional(),
+  no_of_albums_released: Joi.number().optional(),
+});
