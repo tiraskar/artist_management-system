bc85174ce5d6d84af8eec63f0e5e643e760d5492
tribikram <sentribikram@gmail.com>
2024-09-08 11:47:17 +0545

    env variables configuration

--- server/src/config/envConfig.js
+++ server/src/config/envConfig.js
@@ -3,7 +3,10 @@ conf();
 
 const _config = {
     PORT: process.env.PORT || 5000,
-    NODE_ENV: process.env.NODE_ENV || 'development'
+    CORS_WHITELIST: process.env.CORS_WHITELIST,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+    ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY
 };
 
 export const config = Object.freeze(_config);
