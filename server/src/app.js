5114a61c3b325450cd0acd22482bc5857617ac32
tribikram <sentribikram@gmail.com>
2024-09-10 19:11:56 +0545

    modify by removing squalize

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,26 @@
 import express from 'express';
 const app = express();
+import helmet from 'helmet';
+import cors from 'cors';
+import appRoutes from './routes/app.routes.js';
+import globalErrorHandler from './middlewares/globalErrorHandler.js';
+import auth from './middlewares/auth.js';
+
+//for database connection
+import con from './config/database.js';
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+
+app.use(auth);
+app.use(appRoutes);
+
+app.use(globalErrorHandler);
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Not found' });
+});
 
 export default app;
